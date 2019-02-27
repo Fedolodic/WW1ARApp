db1366103884a3887c6af719135bb5b865c44f1b
Fedolodic <jdm150130@utdallas.edu>
2019-02-26 23:05:04 -0600

    yuh

--- ARKitImageRecognition/ViewController.swift
+++ ARKitImageRecognition/ViewController.swift
@@ -2,7 +2,6 @@
 //  ViewController.swift
 //  Image Recognition
 //
-//  Created by Jayven Nhan on 3/20/18.
 //  Copyright © 2018 Jayven Nhan. All rights reserved.
 //
 
@@ -15,13 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     let fadeDuration: TimeInterval = 0.3
-    let rotateDuration: TimeInterval = 3
+    let rotateDuration: TimeInterval = 10
     let waitDuration: TimeInterval = 0.5
     
     lazy var fadeAndSpinAction: SCNAction = {
         return .sequence([
             .fadeIn(duration: fadeDuration),
-            .rotateBy(x: 0, y: 0, z: CGFloat.pi * 360 / 180, duration: rotateDuration),
+            .rotateBy(x: 0, y: CGFloat.pi * 360 / 180, z: 0, duration: rotateDuration),
             .wait(duration: waitDuration),
             .fadeOut(duration: fadeDuration)
             ])
@@ -58,6 +57,186 @@ class ViewController: UIViewController {
         let scaleFactor  = 0.25
         node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
         node.eulerAngles.x += -.pi / 2
+        return node
+    }()
+    
+    lazy var p1: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p2: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p3: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p4: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p5: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p6: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p7: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p8: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p9: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p10: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p11: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p12: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p13: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p14: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p15: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p16: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p17: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p18: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p19: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
+        return node
+    }()
+    
+    lazy var p20: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += .pi*2
         return node
     }()
     
@@ -105,26 +284,78 @@ extension ViewController: ARSCNViewDelegate {
                 let imageName = imageAnchor.referenceImage.name else { return }
             
             // TODO: Comment out code
-            //            let planeNode = self.getPlaneNode(withReferenceImage: imageAnchor.referenceImage)
-            //            planeNode.opacity = 0.0
-            //            planeNode.eulerAngles.x = -.pi / 2
-            //            planeNode.runAction(self.fadeAction)
-            //            node.addChildNode(planeNode)
+            let planeNode = self.getPlaneNode(withReferenceImage: imageAnchor.referenceImage)
+            planeNode.opacity = 0.0
+            planeNode.eulerAngles.x = -.pi/4
+            planeNode.runAction(self.fadeAndSpinAction)
+            node.addChildNode(planeNode)
             
             // TODO: Overlay 3D Object
-            let overlayNode = self.getNode(withImageName: imageName)
-            overlayNode.opacity = 0
-            overlayNode.position.y = 0.2
-            overlayNode.runAction(self.fadeAndSpinAction)
-            node.addChildNode(overlayNode)
+            // let overlayNode = self.getNode(withImageName: imageName)
+            // overlayNode.opacity = 0
+            // overlayNode.position.y = 0.2
+            // overlayNode.runAction(self.fadeAndSpinAction)
+            // node.addChildNode(overlayNode)
             
-            self.label.text = "Image detected: \"\(imageName)\""
+            switch imageName{
+            case "p1":
+                self.label.text = "July 28, 1914"
+            case "p2":
+                self.label.text = "August 1, 1914"
+            case "p3":
+                self.label.text = "August 3, 1914"
+            case "p4":
+                self.label.text = "June 28, 1914"
+            case "p5":
+                self.label.text = "October 31, 1914 to November 7, 1914"
+            case "p6":
+                self.label.text = "September 6, 1914 to September 10, 1914"
+            case "p7":
+                self.label.text = "January 8, 1915"
+            case "p8":
+                self.label.text = "Begins February 1915"
+            case "p9":
+                self.label.text = "Begins March 1915"
+            case "p10":
+                self.label.text = "April 22, 1915"
+            case "p11":
+                self.label.text = "October 12, 1915"
+            case "p12":
+                self.label.text = "February 21, 1916 to Decemeber 18, 1916"
+            case "p13":
+                self.label.text = "April 29, 1916"
+            case "p14":
+                self.label.text = "July 1, 1916 to November 18, 1916"
+            case "p15":
+                self.label.text = "January 5, 1917"
+            case "p16":
+                self.label.text = "April 2, 1917"
+            case "p17":
+                self.label.text = "Novemebr 11, 1918"
+            case "p18":
+                self.label.text = "June 15, 1917"
+            case "p19":
+                self.label.text = "Novemebr 11, 1918"
+            case "p20":
+                self.label.text = "June 28, 1919"
+            default:
+                break
+            }
+            
+            // self.label.text = "Image detected: \"\(imageName)\""
         }
     }
     
     func getPlaneNode(withReferenceImage image: ARReferenceImage) -> SCNNode {
         let plane = SCNPlane(width: image.physicalSize.width,
                              height: image.physicalSize.height)
+        plane.materials = [SCNMaterial()]
+        plane.materials[0].diffuse.contents = UIImage(named: image.name!)
+        // let plane = SCNSphere(radius: image.physicalSize.height)
+        // let material = SCNMaterial()
+        // material.diffuse.contents = UIImage(named: image.name!)
+        // plane.firstMaterial?.diffuse.contents = UIImage(named: image.name!)
+        // plane.materials = [material]
         let node = SCNNode(geometry: plane)
         return node
     }
@@ -138,10 +369,49 @@ extension ViewController: ARSCNViewDelegate {
             node = mountainNode
         case "Trees In the Dark":
             node = treeNode
+        case "p1":
+            node = p1
+        case "p2":
+            node = p2
+        case "p3":
+            node = p3
+        case "p4":
+            node = p4
+        case "p5":
+            node = p5
+        case "p6":
+            node = p6
+        case "p7":
+            node = p7
+        case "p8":
+            node = p8
+        case "p9":
+            node = p9
+        case "p10":
+            node = p10
+        case "p11":
+            node = p11
+        case "p12":
+            node = p12
+        case "p13":
+            node = p13
+        case "p14":
+            node = p14
+        case "p15":
+            node = p15
+        case "p16":
+            node = p16
+        case "p17":
+            node = p17
+        case "p18":
+            node = p18
+        case "p19":
+            node = p19
+        case "p20":
+            node = p20
         default:
             break
         }
         return node
     }
-    
 }
